855c80aa6eae45958062472b739541d9bb6a2d92
THIAGO PROCOPIO DE SOUZA <TPDSOUZ1@corporate.ad>
2026-03-05 11:39:29 -0300

    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import app from "./app"
+import express from "express"
+import cors from "cors"
+import routes from "./routes"
+import { notFoundHandler, errorMiddleware } from "./middlewares/errorMiddleware"
+
+const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: process.env.APP_URL,
+    credentials: true
+}))
+app.use(routes)
+app.use(notFoundHandler)
+app.use(errorMiddleware)
 
 const server = app.listen(port, () => {
     console.log(`🚀 Server running on port ${port}`)
